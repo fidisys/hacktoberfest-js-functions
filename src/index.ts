9d88e2af913c48004f951ee65ace92185098fa88
JayaKrishna <namburu1995@gmail.com>
2019-10-23 20:10:12 +0530

    Adding new fun for random number

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,6 @@ export const getRandomString = () =>
   Math.random()
     .toString(36)
     .substring(2, 15);
+
+export const getRandomNumbers = (max: number, min: number) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
